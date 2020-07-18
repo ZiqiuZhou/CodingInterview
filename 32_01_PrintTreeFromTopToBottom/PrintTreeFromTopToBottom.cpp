fd62fbd947c7c6b3bd618176927172c4d2dacc7f
Dietrich <zzq1063748118@gmail.com>
2020-07-18 15:55:21 +0200

    modify code

--- 32_01_PrintTreeFromTopToBottom/PrintTreeFromTopToBottom.cpp
+++ 32_01_PrintTreeFromTopToBottom/PrintTreeFromTopToBottom.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<queue>
+#include<stack>
 using namespace std;
 
 template<typename Object>
@@ -77,9 +78,62 @@ void printTreeFromTopToBottomPerLine(TreeNode<Object>* root)
 	}
 }
 
+//之字型打印树：第一层从左往右，第二层从右往左，以此类推
+template<typename Object>
+void ZigZagPrintTreeFromTopToBottomPerLine(TreeNode<Object>* root)
+{
+	if (root == nullptr)
+		throw new exception("Invalid Input!");
+	queue<TreeNode<Object>* > queueTreeNode;
+	stack<TreeNode<Object>* > tempSTack;
+	queueTreeNode.push(root);
+	int nextLevel = 0; // 下一层需要打印的节点数量
+	int toBePrinted = 1; //本层还有未打印的节点数量
+	int level = 0; //level为奇数层的节点入栈
+	while (!queueTreeNode.empty())
+	{
+		TreeNode<Object>* poped = new TreeNode<Object>;
+		poped = queueTreeNode.front();
+		if (level % 2 != 0)
+			tempSTack.push(poped);
+		else
+			cout << poped->element << " ";
+		toBePrinted--;
+		queueTreeNode.pop();
+		if (poped->leftChild != nullptr)
+		{
+			queueTreeNode.push(poped->leftChild);
+			nextLevel++;
+		}
+		if (poped->rightChild != nullptr)
+		{
+			queueTreeNode.push(poped->rightChild);
+			nextLevel++;
+		}
+
+		if (toBePrinted == 0)
+		{
+			if(level % 2 == 0)
+				cout << endl;
+			if (level % 2 != 0)
+			{
+				while (!tempSTack.empty())
+				{
+					cout << tempSTack.top()->element << " ";
+					tempSTack.pop();
+				}
+				cout << endl;
+			}
+			toBePrinted = nextLevel;
+			nextLevel = 0;
+			level++;
+		}
+	}
+}
+
 int main()
 {
-	int arr[] = { 2, 3, 1, 7, 3, 5, 0, 8 };
+	int arr[] = { 2, 3, 1, 7, 3, 5, 0, 8, 9 };
 	TreeNode<int>* tree1 = new TreeNode<int>(arr[0]);
 	tree1->leftChild = new TreeNode<int>(arr[1]);
 	tree1->rightChild = new TreeNode<int>(arr[2]);
@@ -88,8 +142,10 @@ int main()
 	tree1->rightChild->leftChild = new TreeNode<int>(arr[5]);
 	tree1->rightChild->rightChild = new TreeNode<int>(arr[6]);
 	tree1->leftChild->rightChild->leftChild = new TreeNode<int>(arr[7]);
+	tree1->leftChild->rightChild->rightChild = new TreeNode<int>(arr[8]);
 	printTreeFromTopToBottom(tree1);
-	cout << endl;
+	cout << endl << endl;
 	printTreeFromTopToBottomPerLine(tree1);
 	cout << endl;
+	ZigZagPrintTreeFromTopToBottomPerLine(tree1);
 }
