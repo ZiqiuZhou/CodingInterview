75c5c248944da2809fcea1df70b724ca18e514e6
Dietrich <zzq1063748118@gmail.com>
2020-07-24 21:29:14 +0200

    add code38

--- 38_StringPermutation/StringPermutation.cpp
+++ 38_StringPermutation/StringPermutation.cpp
@@ -25,9 +25,31 @@ void StringPermutation(string& str)
 	permutationProcess(str, index);
 }
 
+//求数组所有子序列，包括空集
+void StringSubsetProcess(string& str, string res, int index)
+{
+	if (index == str.size())
+	{
+		cout << res << endl;
+		return;
+	}
+		
+	StringSubsetProcess(str, res, index + 1);
+	StringSubsetProcess(str, res + str[index], index + 1);
+}
+
+void StringSubset(string& str)
+{
+	if (str.size() == 0)
+		return;
+	string res = ""; // 结果数组，从空集开始
+	StringSubsetProcess(str, res, 0);
+}
+
 int main()
 {
-	string str = "abcdef";
+	string str = "abc";
 	StringPermutation(str);
+	StringSubset(str);
 	return 0;
 }
