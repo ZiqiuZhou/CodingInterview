cea5b4c0bd099a0f4e350a2cadcf6f03dcee5a96
Dietrich <zzq1063748118@gmail.com>
2020-07-31 22:02:05 +0200

    modify code

--- 57_01_TwoNumbersWithSum/TwoNumbersWithSum.cpp
+++ 57_01_TwoNumbersWithSum/TwoNumbersWithSum.cpp
@@ -5,7 +5,7 @@ using namespace std;
 /*
 题目：在一个递增数组中挑选两个数，使得两数之和等于给定的数
 */
-tuple<int, int> TwoNumbersWithSum(vector<int>& array, int target)
+void TwoNumbersWithSum(vector<int>& array, int target)
 {
 	if (array.empty())
 		throw new exception("Invalid input!");
@@ -14,24 +14,22 @@ tuple<int, int> TwoNumbersWithSum(vector<int>& array, int target)
 	while (leftPtr < rightPtr)
 	{
 		int sum = array[leftPtr] + array[rightPtr];
-		if (sum == target)
-		{
-			//cout << array[leftPtr] << " " << array[rightPtr];
-			return std::make_tuple(array[leftPtr], array[rightPtr]);
-		}
-		else if (sum > target)
+		if (sum > target)
 			rightPtr--;
+		else if (sum < target)
+			leftPtr--;
 		else
-			leftPtr++;
+		{
+			cout << array[leftPtr] << " " << array[rightPtr];
+			break;
+		}
 	}
 }
 
 int main()
 {
 	vector<int> arr{1, 2, 3, 4, 5, 6, 7, 8, 9};
-	tuple<int, int> a;
-	a = TwoNumbersWithSum(arr, 6);
-	cout << std::get<0>(a) << " "<< std::get<1>(a) ;
+	TwoNumbersWithSum(arr, 6);
 	return 0;
 }
 
